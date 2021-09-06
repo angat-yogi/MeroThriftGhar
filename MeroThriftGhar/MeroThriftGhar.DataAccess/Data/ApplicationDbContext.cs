a0365a4dda6965aefa3b31674c876f82dc770b42
Angat Yogi <angat.yogi@ou.edu>
2021-09-06 17:13:32 -0500

    Connected To Database using SQL Server.

--- MeroThriftGhar/MeroThriftGhar.DataAccess/Data/ApplicationDbContext.cs
+++ MeroThriftGhar/MeroThriftGhar.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MeroThriftGhar.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
