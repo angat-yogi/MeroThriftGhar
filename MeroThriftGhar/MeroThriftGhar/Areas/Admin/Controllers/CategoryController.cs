0f02a8b34cebc6e794333773509d9ecab56efc5b
Angat Yogi <angat.yogi@ou.edu>
2021-09-09 18:32:30 -0500

    _EditAndBackToListButton.cshtml and _CreateAndBackToListButton.cshtml created in partial view

--- MeroThriftGhar/MeroThriftGhar/Areas/Admin/Controllers/CategoryController.cs
+++ MeroThriftGhar/MeroThriftGhar/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using MeroThriftGhar.DataAccess.Repository.IRepository;
+using MeroThriftGhar.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,24 @@ namespace MeroThriftGhar.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+
+            if (id==null)
+            {
+                //this is for create
+                return View(category);
+            }
+            //this is for edit
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
 
         //Java API Calls
         #region API CALLS
